4348b9fa7ccb6aae04c73196aea56708ae3765a0
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2024-02-20 14:47:19 +0530

    upadting components

--- src/components/adhoc.js
+++
@@ -1,5 +0,0 @@
-/* for shimmer ui :
-1. on click on button in gpt search bar... shimmer should come and once we get 
-data shimmer should go
-2. make shimmer var in store. onclick on seach button make this true
-3. subscibe shimmer & define your shimmer ui
